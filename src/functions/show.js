291e5e3100d4831e68052a215b53300294046831
Wilson Gomes <contato@wilsonneto.com.br>
2020-01-19 23:23:30 -0300

    refatorando para separar as functions em arquivos no src

---
+++ src/functions/show.js
@@ -0,0 +1,41 @@
+const azure = require("azure-storage");
+const tableName = "cotacao";
+
+module.exports = {
+  run: async (context, req) => {
+    const tableService = azure.createTableService(
+      process.env.AzureWebJobsStorage
+    );
+
+    if (req.query.acao || (req.body && req.body.acao)) {
+      const query = new azure.TableQuery()
+        .top(25)
+        .where("PartitionKey eq ?", req.query.acao);
+
+      const options = { payloadFormat: "application/json;odata=nometadata" };
+
+      const entries = await new Promise((resolve, reject) => {
+        tableService.queryEntities(tableName, query, null, options, function(
+          error,
+          result,
+          response
+        ) {
+          if (!error) {
+            resolve(response.body);
+          }
+          reject(error);
+        });
+      });
+
+      context.res = {
+        status: 200,
+        body: JSON.stringify(entries)
+      };
+    } else {
+      context.res = {
+        status: 400,
+        body: "Passe a acao por parametro para que seja mostrado o valor"
+      };
+    }
+  }
+};
